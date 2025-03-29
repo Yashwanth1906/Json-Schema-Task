236a5f6c3d06bdb04b8d5788199d64d2d7ca98b0
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2025-03-29 08:49:46 +0530

    Added vercel.json file for the backend

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,16 @@ import { normalContentRouter } from "./routes/normalContentRoute.js";
 import cors from "cors"
 const app = express();
 app.use(express.json());
-app.use(cors());
-const PORT = process.env.PORT || 6969;
+app.use(cors({
+  allowCredentials: true,
+  origin: ["http://localhost:5173", "https://json-schema-task-frontend.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Content-Range", "X-Content-Range"],
+  credentials: true,
+  maxAge: 86400,
+}));
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
