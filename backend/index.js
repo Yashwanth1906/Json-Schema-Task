b0322ab37c33208222e88780474b6dc44096b06b
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2025-03-29 09:23:15 +0530

    Changed the crediantials key.json file to be created at runtime in vercel

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,17 @@ import { docRouter } from "./routes/docRoute.js";
 import { tableContentRouter } from "./routes/tableContentRoute.js";
 import { normalContentRouter } from "./routes/normalContentRoute.js";
 import cors from "cors"
+
+/// This logic is for vercel deployment
+const keyFilePath = "/tmp/key.json";
+
+// Write the credentials to key.json if not already present
+if (process.env.GOOGLE_CREDENTIALS) {
+  fs.writeFileSync(keyFilePath, process.env.GOOGLE_CREDENTIALS);
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = keyFilePath;
+}
+// This logic is for vercel deployment
+
 const app = express();
 app.use(express.json());
 app.use(cors({
