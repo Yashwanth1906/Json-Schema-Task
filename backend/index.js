192d099953fce1caff530bd74538738dab9eafcd
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2025-03-29 12:51:09 +0530

    Added frontend url for cors in backend

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   allowCredentials: true,
-  origin: ["http://localhost:5173", "https://json-schema-task-frontend.vercel.app/"],
+  origin: ["http://localhost:5173", "https://json-schema-task-frontend.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   exposedHeaders: ["Content-Range", "X-Content-Range"],
